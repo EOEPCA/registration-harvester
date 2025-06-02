8b39413f471f7aabffb30146c8f62f4596b0e4b6
Mario Winkler <mario.winkler@dlr.de>
2025-06-02 11:10:23 +0200

    WIP: IAM integration

--- src/worker/common/secrets.py
+++ src/worker/common/secrets.py
@@ -11,10 +11,12 @@ class WorkerSecrets:
         self.secrets["m2m_password"] = os.environ["M2M_PASSWORD"] if "M2M_PASSWORD" in os.environ else ""
         self.secrets["flowable_user"] = os.environ["FLOWABLE_USER"] if "FLOWABLE_USER" in os.environ else ""
         self.secrets["flowable_password"] = os.environ["FLOWABLE_PASSWORD"] if "FLOWABLE_PASSWORD" in os.environ else ""
-        self.secrets["iam_client_id"] = os.environ["IAM_CLIENT_ID"] if "IAM_CLIENT_ID" in os.environ else ""
-        self.secrets["iam_client_secret"] = os.environ["IAM_CLIENT_SECRET"] if "IAM_CLIENT_SECRET" in os.environ else ""
+        self.secrets["iam_client_id"] = os.environ["IAM_CLIENT_ID"] if "IAM_CLIENT_ID" in os.environ else None
+        self.secrets["iam_client_secret"] = (
+            os.environ["IAM_CLIENT_SECRET"] if "IAM_CLIENT_SECRET" in os.environ else None
+        )
 
-    def get_secret(self, key: str, default):
+    def get_secret(self, key: str, default: str):
         if key is not None and len(key) > 0:
             return self.secrets[key]
         else:
