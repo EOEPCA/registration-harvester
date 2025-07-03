8c9c224e313df146076abebcbf7bcf1d0408fa68
Mario Winkler <mario.winkler@dlr.de>
2025-07-03 16:01:03 +0200

    Add tutorial files

---
+++ src/worker/tutorial/tasks.py
@@ -0,0 +1,59 @@
+# src/worker/tutorial/tasks.py
+from flowable.external_worker_client import ExternalJob, JobResult, JobResultBuilder
+
+from worker.common.log_utils import log_with_context  # For logging
+from worker.common.task_handler import TaskHandler
+
+
+class TutorialDiscoverItemsTaskHandler(TaskHandler):
+    def execute(self, job: ExternalJob, result: JobResultBuilder, config: dict) -> JobResult:
+        log_context = {"JOB": job.id, "BPMN_TASK": job.element_name}
+        log_with_context("Starting DiscoverItems task ...", log_context)
+
+        try:
+            # no input data needed for this task
+
+            # get STAC API url from configuration
+            api_url = self.get_config("service_url", "http://default.api.com")
+
+            # 2. Perform task logic
+            log_with_context(f"Searching STAC items using API: {api_url}", log_context)
+            # stac search
+            items = []
+
+            # 3. Return success with output variables
+            log_with_context("DiscoverItems task completed successfully.", log_context)
+            return result.success().variable_string(name="items", value=items)
+
+        except Exception as e:
+            error_msg = str(e)
+            log_with_context(error_msg, log_context, "error")
+            # Use the task_failure helper for consistent error reporting
+            return self.task_failure("Error in TutorialDiscoverItemsTaskHandler", error_msg, result)
+
+
+class TutorialProcessItemTaskHandler(TaskHandler):
+    def execute(self, job: ExternalJob, result: JobResultBuilder, config: dict) -> JobResult:
+        log_context = {"JOB": job.id, "BPMN_TASK": job.element_name}
+        log_with_context("Starting ProcessItem task ...", log_context)
+
+        try:
+            # 1. Get input variables
+            item = job.get_variable("item")
+
+            if not item:
+                log_with_context("Missing 'item' input variable.", log_context, "error")
+                return result.failure().error_message("Input data is missing.")
+
+            # 2. Perform task logic: just logging the item
+            log_with_context(f"Processing item {item}", log_context)
+
+            # 3. Return success, no output variable produced by this task
+            log_with_context("ProcessItem task completed successfully.", log_context)
+            return result.success()
+
+        except Exception as e:
+            error_msg = str(e)
+            log_with_context(error_msg, log_context, "error")
+            # Use the task_failure helper for consistent error reporting
+            return self.task_failure("Error in TutorialProcessItemTaskHandler", error_msg, result)
