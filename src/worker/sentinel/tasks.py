5f10ef720d207e5aa795c5c1dd6c1315068611d4
Mario Winkler <mario.winkler@dlr.de>
2026-07-21 21:42:13 +0200

    Remove unused code

--- src/worker/sentinel/tasks.py
+++ src/worker/sentinel/tasks.py
@@ -3,7 +3,6 @@ import time
 import zipfile
 from pathlib import Path
 
-import requests
 from eodag import EODataAccessGateway, EOProduct
 from operaton.external_task.external_task import ExternalTask, TaskResult
 
@@ -11,7 +10,6 @@ from worker.common.datasets import sentinel
 from worker.common.log_utils import configure_logging, format_duration, format_file_metrics, log_with_context
 from worker.common.resources import stac
 from worker.common.search_interval import determine_search_interal
-from worker.common.secrets import worker_secrets
 from worker.common.task_handler import TaskHandler
 
 configure_logging()
@@ -245,81 +243,6 @@ class SentinelDownloadHandler(TaskHandler):
     def _get_scene_path(self, base_dir, scene):
         zip_path = Path(scene["id"].lstrip("/") + ".zip")
         return str(Path(base_dir) / zip_path)
-
-    def _get_access_token(self):
-        if "token_expire_time" in os.environ and time.time() <= (float(os.environ["token_expire_time"]) - 5):
-            return os.environ["s3_access_key"]
-
-        cdse_user = worker_secrets.get_secret("cdse_user", "")
-        cdse_password = worker_secrets.get_secret("cdse_password", "")
-        auth_server_url = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
-        data = {
-            "client_id": "cdse-public",
-            "grant_type": "password",
-            "username": cdse_user,
-            "password": cdse_password,
-        }
-
-        token_time = time.time()
-        response = requests.post(auth_server_url, data=data, verify=True, allow_redirects=False).json()
-        os.environ["token_expire_time"] = str(token_time + response.get("expires_in", 0))
-        os.environ["s3_access_key"] = response.get("access_token", "")
-        return os.environ["s3_access_key"]
-
-    def _download_data(
-        self,
-        url,
-        output_dir,
-        file_name=None,
-        chunk_size=1024 * 1000,
-        timeout=300,
-        auth=None,
-        check_size=True,
-        overwrite=False,
-    ):
-        """
-        Download single file from USGS M2M by download url
-        """
-
-        if auth:
-            r = requests.get(url, stream=True, allow_redirects=True, timeout=timeout, auth=auth)
-        else:
-            r = requests.get(url, stream=True, allow_redirects=True, timeout=timeout)
-        r.raise_for_status()
-
-        expected_file_size = int(r.headers.get("content-length", -1))
-        if file_name is None:
-            try:
-                file_name = r.headers["Content-Disposition"].split('"')[1]
-            except Exception:
-                file_name = os.path.basename(url)
-                # raise Exception("Can not automatically identify file_name.")
-
-        file_path = os.path.join(output_dir, file_name)
-        # TODO: Check for existing files and whether they have the correct file size
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-
-        if os.path.exists(file_path) and not overwrite:
-            return file_path
-        elif os.path.exists(file_path) and overwrite:
-            os.remove(file_path)
-
-        with open(file_path, "wb") as f:
-            # start = time.perf_counter()
-            for chunk in r.iter_content(chunk_size=chunk_size):
-                f.write(chunk)
-            # duration = time.perf_counter() - start
-
-        file_size = os.stat(file_path).st_size
-        # speed = round((file_size / duration) / (1000 * 1000), 2)
-
-        if check_size:
-            if expected_file_size != file_size:
-                os.remove(file_path)
-                raise Exception(f"Failed to download from {url}")
-
-        return file_path
 
     @staticmethod
     def _create_generic_stac_item(_id: str) -> dict:
